149ab40e460397c2b794ad42f9442c043395f5ee
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-06-15 18:11:53 +0200

    box init

--- dad-jokes-app/src/App.js
+++ dad-jokes-app/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <p>Do not laugh challenge</p>
+        <p>Random dad joke</p>
+        <button>Get another joke</button>
+      </div>
     </div>
   );
 }
